a236964ae8d731b3dc816e2a8a96af47ee02fd8d
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-08-24 22:39:42 +0000

    Done.

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,20 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+    begin
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |request|
+        request.params['q'] = params[:query]
+      end
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @repos = body["items"]
+      else
+        @error = "There was an error"
+      end
+    rescue Faraday::ConnectionFailed
+      @error = "Your request timed out. Please try again"
+    end
+    render 'search'
   end
+
 end
